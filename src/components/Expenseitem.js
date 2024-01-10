ec8c3edf0a4e384fab7cd4e6e1bbfbb5439041b7
DebasishKisan1 <debasish.kisan@gmail.com>
2024-01-10 12:28:42 +0530

    css and variable

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,15 +1,18 @@
-function Expenseitem(){
+import './Expenseitem.css'
+
+function Expenseitem() {
+    const expenseDate = new Date(2021, 3, 15)
+    const expenseDescription = 'Car Insurance'
+    const expenseAmount = '$294.67'
+    const locationOfExpenditure = 'New York'
     return (
-        <div>
-            <h4>
-                Food Rs 10
-            </h4>
-            <h4>
-                Petrol Rs 100
-            </h4>
-            <h4>
-                Movies Rs 200
-            </h4>
+        <div className='expense-item'>
+            <div> {expenseDate.toISOString()} </div>
+            <div className='expense-item__description'>
+                <h2>{expenseDescription}</h2>
+                <h2>{locationOfExpenditure}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
+            </div>
         </div>
     )
 }
